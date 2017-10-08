43241b6d2b038cfd9cb599c4d9a14b6c572c1552
Drew Mercer <japanexpress@gmail.com>
2017-10-08 18:45:50 -0400

    Updating bike model

--- models/bike.js
+++ models/bike.js
@@ -1,29 +1,28 @@
 module.exports = function(sequelize, DataTypes) {
-    var Bike = sequelize.define("Bike", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      body: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1]
+  var Bike = sequelize.define('Bike', {
+    bike_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bike_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bike_miles: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  });
+
+  Bike.associate = function(models) {
+    // We're saying that a Post should belong to an Owner
+    // A Post can't be created without an Owner due to the foreign key constraint
+    Bike.belongsTo(models.Owner, {
+      foreignKey: {
+        allowNull: false
       }
     });
-  
-    Bike.associate = function(models) {
-      // We're saying that a Post should belong to an Owner
-      // A Post can't be created without an Owner due to the foreign key constraint
-      Bike.belongsTo(models.Owner, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-  
-    return Bike;
   };
-  
+
+  return Bike;
+};
