f81f99586c2eb3ea7921a664bce7d3db61944da1
Drew Mercer <japanexpress@gmail.com>
2017-10-08 18:02:45 -0400

    Setting up initial owner model

---
+++ models/owner.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+    var Owner = sequelize.define("Owner", {
+      // Giving the Owner model a name of type STRING
+      name: DataTypes.STRING
+    });
+  
+    Owner.associate = function(models) {
+      // Associating Owner with Bikes
+      // When an Owner is deleted, also delete any associated Bikes
+      Owner.hasMany(models.bicycle, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return Owner;
+  };
+  
