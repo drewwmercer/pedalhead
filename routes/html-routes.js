a8668d725b52b4d71cbe1c7e2e58c0f585aa8571
Drew Mercer <japanexpress@gmail.com>
2017-10-16 02:48:39 -0400

    Updates across multiple files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -35,44 +35,50 @@ module.exports = function(app) {
 
   // my-garage views your current bikes
   app.get('/my-garage', function(req, res) {
-    db.Bike.findAll({}).then(function(bikeres) {
-      res.render('mygarage', { bikes: bikeres });
-    });
+    db.Bike
+      .findAll({
+        include: [{ model: db.Owner }]
+        // where: {OwnerId: 1}}).then(function(bikeres) {
+      })
+      .then(function(bikeres) {
+        res.render('mygarage', { bikes: bikeres });
+      });
   });
 
   // api/tables views all of the data as json
   app.get('/api/tables', function(req, res) {
     db.Bike.findAll({}).then(function(bikedata) {
       res.json(bikedata);
-      
-    });
-    db.Owner.findAll({}).then(function(ownerdata) {
-      res.json(ownerdata);
     });
   });
-    // authors route loads owner-manager.html
-    app.get('/service-manager', function(req, res) {
-      res.sendFile(path.join(__dirname, '../public/ServMngr.html'));
-    });
-  
+  // authors route loads owner-manager.html
+  app.get('/service-manager', function(req, res) {
+    //res.sendFile(path.join(__dirname, '../public/ServMngr.html'));
+    res.render('servicemanager');
+  });
 
   // POST route for adding a new bike
   app.post('/api/addnewbike', function(req, res) {
     console.log(req.body);
-    db.Bikes
+    db.Bike
       .create({
         bike_name: req.body.bike_name,
         bike_type: req.body.bike_type,
-        purchase_date: purchase_date,
-        OwnerId: auth2.currentUser
-          .get()
-          .getBasicProfile()
-          .profile.getId()
+        bike_miles: req.body.bike_miles,
+        purchase_date: req.body.purchase_date
+        // OwnerId: auth2.currentUser
+        //   .get()
+        //   .getBasicProfile()
+        //   .profile.getId()
       })
-      .then(function(dbBikeAdd) {
-        // We have access to the new burger as an argument inside of the callback function
-        res.json(dbBikeAdd);
+      .then(function(result) {
+        var responseObj = {
+          Id: result.dataValues.id
+        };
+        res.status(200).json(responseObj);
+      })
+      .catch(function(err) {
+        res.status(500).json(err);
       });
   });
 };
-
