dfdff3669dbac3b9ef0eaea9568d42b70ca98fa1
Drew Mercer <japanexpress@gmail.com>
2017-10-16 21:40:04 -0400

    Removing assocation to bikes

--- models/owner.js
+++ models/owner.js
@@ -1,24 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
-    var Owner = sequelize.define("Owner", {
-      // Giving the Owner model a name of type STRING
-      owner_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      owner_token: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      }
-    });
-  
-    Owner.associate = function(models) {
-      // Associating Owner with Bikes
-      // When an Owner is deleted, also delete any associated Bikes
-      Owner.hasMany(models.Bike, {
-        onDelete: "cascade"
-      });
-    };
-  
-    return Owner;
-  };
-  
+  var Owner = sequelize.define('Owner', {
+    // Giving the Owner model a name of type STRING
+    owner_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    owner_token: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  return Owner;
+};
