7d2bd0afd16bcba3bf2978dde09ddd194fe7bdcf
Drew Mercer <japanexpress@gmail.com>
2017-10-16 21:11:03 -0400

    Updating bike db model

--- models/bike.js
+++ models/bike.js
@@ -14,18 +14,14 @@ module.exports = function(sequelize, DataTypes) {
     },
     purchase_date: {
       type: DataTypes.DATEONLY
+    },
+    owner_name: {
+      type: DataTypes.STRING
+    },
+    owner_token: {
+      type: DataTypes.STRING
     }
   });
-
-  // Bike.associate = function(models) {
-  //   // We're saying that a Bike should belong to an Owner
-  //   // A Bike can't be created without an Owner due to the foreign key constraint
-  //   Bike.belongsTo(models.Owner, {
-  //     foreignKey: {
-  //       allowNull: false
-  //     }
-  //   });
-  // };
 
   return Bike;
 };
